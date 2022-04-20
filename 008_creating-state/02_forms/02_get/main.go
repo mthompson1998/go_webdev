bb65bd75ab156ae27c3668b640d2cf6c15549521
Michael Thompson <michael.thompson@redwirespace.com>
2022-04-20 15:59:26 -0400

    doing great

---
+++ 008_creating-state/02_forms/02_get/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	v := r.FormValue("q")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+	<form method="get">
+	<input type="text" name="q">
+	<input type="submit">
+	</form>
+	<br>`+v)
+}
+
+/*
+if method is POST -- the info and values will be passed through the body
+
+if method is GET -- the info and values will be passed through the URL
+
+*/
