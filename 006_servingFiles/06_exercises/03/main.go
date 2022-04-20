06d52450bdabfa2f7d39ad760b007626b11eaa4e
Mike Thompson <mtthompson11@gmail.com>
2022-04-20 14:30:38 -0400

    doing great

---
+++ 006_servingFiles/06_exercises/03/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("template/index.html"))
+}
+
+func main() {
+	http.HandleFunc("/", dogs)
+	http.Handle("/resources", http.StripPrefix("/resources", http.FileServer(http.Dir("public"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+func dogs(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, nil)
+	if err != nil {
+		log.Fatalln("template didnt execute", err)
+	}
+
+}
